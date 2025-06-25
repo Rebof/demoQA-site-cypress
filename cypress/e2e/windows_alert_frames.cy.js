ca7e48a51dd9c62b4406c5002a80ccd18618b71d
Rebof <rebofkatwal7@gmail.com>
2025-06-25 14:37:08 +0545

    window tabs testing done

--- cypress/e2e/windows_alert_frames.cy.js
+++ cypress/e2e/windows_alert_frames.cy.js
@@ -1,5 +1,41 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
+});
+
+beforeEach(() => {
+  cy.clearAllCookies();
+  cy.clearAllSessionStorage();
+  cy.clearAllLocalStorage();
+  cy.visit("/");
+});
+
+describe("new windows and alerts", () => {
+  it("tests all window behaviors", () => {
+    cy.PageCheck("browser-windows", "Browser Windows");
+
+    cy.window().then((win) => {
+      cy.stub(win, "open").as("windowOpen");
+    });
+  
+    // Test tab button opens /sample
+    cy.get("#tabButton").click();
+    cy.get("@windowOpen").should("be.calledWith", "/sample");
+  
+    // Reset the stub's history before next use
+    cy.get("@windowOpen").invoke('resetHistory');
+  
+    // Test window button opens /sample
+    cy.get("#windowButton").click();
+    cy.get("@windowOpen").should("be.calledWith", "/sample");
+  
+    cy.get("@windowOpen").invoke('resetHistory');
+  
+    // Test message window button calls window.open (URL unknown, so just check called)
+    cy.get("#messageWindowButton").click();
+    cy.get("@windowOpen").should("be.called");
+  });
+  
+  it('alerts', () => {
+    
+  });
+});
