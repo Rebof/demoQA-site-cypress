f1e413fd43ae8db3bd9366d10af6d6230f992f79
Rebof <rebofkatwal7@gmail.com>
2025-06-26 11:19:02 +0545

    windows alert section complete(modals element handling)

--- cypress/e2e/windows_alert_frames.cy.js
+++ cypress/e2e/windows_alert_frames.cy.js
@@ -73,7 +73,7 @@ describe("new windows and alerts", () => {
  
   });
   
-  it.only('iframe body without the nests', () => {
+  it('iframe body without the nests', () => {
     cy.PageCheck("frames", "Frames");
 
     cy.wait(5000)
@@ -86,5 +86,40 @@ describe("new windows and alerts", () => {
 
 
   });
-  
+
+  it("iframe with nests", () => {
+    cy.PageCheck("nestedframes", "Nested Frames");
+
+    cy.wait(5000);
+    cy.getIframeBody("#frame1")
+      .within(()=>{
+        cy.get("iframe") 
+          .its("0.contentDocument")
+          .should("exist")
+          .its("body")
+          .should("not.be.empty")
+          .then(cy.wrap)
+          .find("p") 
+          .should("contain", "Child Iframe");
+      });
+  });
+
+it.only('modal dialogs', () => {
+  cy.PageCheck("modal-dialogs", "Modal Dialogs");
+
+  cy.get("#showLargeModal").click();
+  cy.get(".modal-body")
+    .first()
+    .should(
+      "contain",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    );
+  // there a p tag here
+  cy.get("#closeLargeModal").click();
+
+  cy.get("#showSmallModal").click();
+  cy.get('#example-modal-sizes-title-sm')
+    .should("contain", "Small Modal");
+  cy.get("#closeSmallModal").click();
+});   
 });
